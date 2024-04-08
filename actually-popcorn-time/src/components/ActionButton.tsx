bd264f690d1ea4b93f5996802f41be4fcbd0e4ec
cherylau <clau136@my.bcit.ca>
2024-04-07 23:59:24 -0700

    UI Changes

--- actually-popcorn-time/src/components/ActionButton.tsx
+++ actually-popcorn-time/src/components/ActionButton.tsx
@@ -1,15 +1,27 @@
 import { useNavigate } from "react-router-dom";
+
+import { ButtonHTMLAttributes } from "react";
+
 type ActionButtonProps = {
   buttonText: string;
-  route: string | undefined;
-  className: string | undefined;
-};
+  route?: string;
+  className?: string;
+  outline?: boolean;
+  children?: React.ReactNode;
+  icon?: string;
+  onClick?: () => void;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
-const ActionButton = ({ buttonText, route, className }: ActionButtonProps) => {
+const ActionButton = ({ buttonText, route, className, outline }: ActionButtonProps) => {
   const navigate = useNavigate();
   return (
     <button
-      className={`${className} btn rounded-full bg-action text-primary`}
+      type="button"
+      className={`btn flex items-center gap-1 ${className} ${
+        outline
+          ? " btn-outline btn-sm rounded-full border-primary text-primary"
+          : "btn rounded-full border-action bg-action text-primary"
+      }`}
       onClick={() => route && navigate(route)}
     >
       {buttonText}
