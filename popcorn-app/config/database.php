011082319e89f96f6c2ef53942a5f2d9b01284db
cherylau <clau136@my.bcit.ca>
2024-04-10 18:38:49 -0700

    Add database config line for deployment

--- popcorn-app/config/database.php
+++ popcorn-app/config/database.php
@@ -34,8 +34,8 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
-            // 'database' => env('DB_DATABASE', database_path('movies.sqlite')),
-            'database' => database_path(env('DB_DATABASE')),
+            'database' => env('DB_DATABASE', database_path('movies.sqlite')),
+            // 'database' => database_path(env('DB_DATABASE')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
